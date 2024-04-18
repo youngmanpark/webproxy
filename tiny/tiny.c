0eedb779f010e509f755c09e47b36bfc8033d404
youngmanpark <py980627@gmail.com>
2024-04-18 16:29:22 +0000

    read_requesthdrs 구현

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -116,3 +116,16 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
     Rio_writen(fd, body, strlen(body));
 }
 
+/*
+*
+*/
+void read_requesthdrs(rio_t *rp) {
+    char buf[MAXLINE];
+
+    Rio_readlineb(rp, buf, MAXLINE);
+    while (strcmp(buf, "\r\n")) {
+        Rio_readlineb(rp, buf, MAXLINE);
+        printf("%s", buf);
+    }
+    return;
+}
