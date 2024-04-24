5b5ea1a7631c771a2ad357eff8973b9615fb79e8
youngmanPark <py980627@gmail.com>
2024-04-24 17:03:20 +0900

    cache 수정

--- cache.c
+++ cache.c
@@ -47,14 +47,8 @@ void update_cache(web_object *web_ob) {
         return;
     // 캐시리스트에서 객체가 마지막이 아닐 경우
     if (web_ob->next != NULL) {
-        web_object *prev_ob = web_ob->prev; // 객체의 이전 객체
-        web_object *next_ob = web_ob->next; // 객체의 다음 객체
-
-        // 이전 객체가 없을 경우
-        if (prev_ob != NULL)
-            web_ob->prev->next = next_ob;
-        else
-            web_ob->next->prev = prev_ob;
+        web_ob->prev->next = web_ob->next;
+        web_ob->next->prev = web_ob->prev;
     }
     // 캐시리스트에서 객체가 마지막일 경우
     else {
@@ -62,7 +56,9 @@ void update_cache(web_object *web_ob) {
     }
 
     web_ob->next = root; // 기존의 맨처음 객체(root)는 현재 객체의 다음 객체로(web_ob->next)
+    root->prev = web_ob; // 기존의 맨처음 객체(root)의 이전 객체를 현재 객체로(web_ob)
     root = web_ob;       // 캐시리스트의 맨처음 객체(root)로 현재 객체(web_ob) 설정
+    web_ob->prev = NULL; // 현재 객체(web_ob)의 이전을 NULL
 }
 
 /* 웹 객체 캐싱 리스트에 등록 함수*/
@@ -73,8 +69,8 @@ void regi_cache(web_object *web_ob) {
         total_cache_size -= tail->content_len; // 총 캐시 리스트의 크기를 마지막 객체의 크기만큼 빼기
         tail = tail->prev;                     // 마지막 캐시를 마지막 이전 캐시로 변경
         free(tail->next->response_ptr);
-        free(tail->next);                      // 제거한 캐시데이터 메모리 반환
-        tail->next = NULL;                     // 변경된 마지막 캐시 데이터의 다음을 NULL(끝임을 알려줌)로 변경
+        free(tail->next);  // 제거한 캐시데이터 메모리 반환
+        tail->next = NULL; // 변경된 마지막 캐시 데이터의 다음을 NULL(끝임을 알려줌)로 변경
     }
     // 빈 캐시리스트일 경우 root,tail에 객체 담기
     if (root == NULL) {
